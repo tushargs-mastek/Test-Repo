9eca77257f4feef64950769261d8d306c1d6789b
tushargs-mastek <tushar.shinde@mastek.com>
2016-01-25 15:45:35 +0530

    Update2

---
+++ src/net/codejava/spring/TestClass2.java
@@ -0,0 +1,10 @@
+package net.codejava.spring;
+
+
+public class TestClass2{
+	
+	public static void main(String a[]){
+		System.out.println("This is a Test class");
+		System.out.println("This is a Test class2");
+	}
+}
