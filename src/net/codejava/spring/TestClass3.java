1257085e246b9d51f2bd66a5485791f963a12de2
Tushar G. Shinde <tushar13504@Mastek.com>
2016-01-25 15:56:46 +0530

    Update4

---
+++ src/net/codejava/spring/TestClass3.java
@@ -0,0 +1,11 @@
+package net.codejava.spring;
+
+
+public class TestClass3{
+	
+	public static void main(String a[]){
+		System.out.println("This is a Test class");
+		System.out.println("This is a Test class3");
+		System.out.println("This is a NEW Test class3");
+	}
+}
