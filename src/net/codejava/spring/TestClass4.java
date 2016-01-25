2f22fb541292ad7838fce7e2ea9f936cc987ee5f
Tushar G. Shinde <tushar13504@Mastek.com>
2016-01-25 16:34:03 +0530

    Update5

---
+++ src/net/codejava/spring/TestClass4.java
@@ -0,0 +1,11 @@
+package net.codejava.spring;
+
+
+public class TestClass4{
+	
+	public static void main(String a[]){
+		System.out.println("This is a Test class");
+		System.out.println("This is a Test class4");
+		System.out.println("This is a NEW Test class4");
+	}
+}
